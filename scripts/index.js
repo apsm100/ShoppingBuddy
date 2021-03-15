0622235037fdf2b6aa9b22089e7e68271d2dcf18
apsm100 <amritmanhas11@gmail.com>
2021-03-15 15:05:03 -0700

    Connect firestore to search; index search only

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,9 @@ var mainSearch = document.getElementById('main-search');
 var topNavBar = document.getElementById("top-nav-bar");
 var topSearchBar = document.getElementById("top-search-bar");
 var topSearchBarInput = document.getElementById("search-input");
-
 var searchContainer = document.getElementById("search-container");
-
 var searchSuggestion = document.getElementById("suggestions");
-var searchResult = document.getElementById("results");
+
 function searchStart() {
     mainSearch.style.display = 'none';
     topNavBar.style.display = 'none';
@@ -18,8 +16,50 @@ function searchStart() {
 function searchTyping() {
     mainContainer.style.display = 'none';
     searchContainer.style.display = "block";
-    searchResult.style.display = 'none';
+    searchdb(topSearchBarInput.value);
 }
 function suggestionClick(suggestion) {
     topSearchBarInput.value = suggestion;
+    searchdb(suggestion);
+}
+
+function searchdb(item) {
+    db.collection("search")
+        .get()
+        .then(function (snap) {
+            searchSuggestion.innerHTML = "";
+            snap.forEach(function (doc) {
+                // console.log(doc.data());
+                //do something with the data
+                if (doc.data().query.toUpperCase().indexOf(item.toUpperCase()) > -1) {
+                    populateSearch(doc.data());
+                  }
+                
+            })
+        })
+}
+function populateSearch(item) {
+    a = document.createElement("div");
+    a.setAttribute("class", "card");
+    a.setAttribute("id", "search-item");
+    a.setAttribute("onclick", "suggestionClick('" + item.query + "')");
+    b = document.createElement("div");
+    b.setAttribute("id", "suggestion-item");
+
+    itemName = document.createElement("span");
+    itemName.setAttribute("id", "search-item-name");
+
+    b.setAttribute("class", "card-body");
+    c = document.createElement("div");
+    c.setAttribute("id", "search-item-category");
+
+    itemName.innerHTML = item.query;
+    c.innerHTML = item.category;
+
+    b.appendChild(itemName);
+    b.appendChild(c);
+    a.appendChild(b);
+
+    searchSuggestion.appendChild(a);
+    
 }
