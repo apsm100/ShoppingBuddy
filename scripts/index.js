c236d13ca4c0175fbd056804c69be68645c53b4d
apsm100 <amritmanhas11@gmail.com>
2021-03-24 11:14:34 -0700

    Remove extra js stuff from index.js

--- scripts/index.js
+++ scripts/index.js
@@ -209,60 +209,9 @@ function sayHello(){
             .then(function(doc){
                 console.log(doc.data().name);
                 var n = doc.data().name;
-                $("#Name-goes-here").text(n);
+                $("#Name-goes-here").text(n + "'s ");
             })
         }
     })
 }
 sayHello();
-
-function sayEmail(){
-    firebase.auth().onAuthStateChanged(function(somebody){
-        if(somebody){
-            console.log(somebody.uid)
-            db.collection("users")
-            .doc(somebody.uid)
-            .get()
-            .then(function(doc){
-                console.log(doc.data().email);
-                var n = doc.data().email;
-                $("#Email-goes-here").text(n);
-            })
-        }
-    })
-}
-sayEmail();
-
-function sayAddress(){
-    firebase.auth().onAuthStateChanged(function(somebody){
-        if(somebody){
-            console.log(somebody.uid)
-            db.collection("users")
-            .doc(somebody.uid)
-            .get()
-            .then(function(doc){
-                console.log(doc.data().address);
-                var n = doc.data().address;
-                $("#Address-goes-here").text(n);
-            })
-        }
-    })
-}
-sayAddress();
-
-function sayDeliveryInstructions(){
-    firebase.auth().onAuthStateChanged(function(somebody){
-        if(somebody){
-            console.log(somebody.uid)
-            db.collection("users")
-            .doc(somebody.uid)
-            .get()
-            .then(function(doc){
-                console.log(doc.data().deliveryInstructions);
-                var n = doc.data().deliveryInstructions;
-                $("#DeliveryInstructions-goes-here").text(n);
-            })
-        }
-    })
-}
-sayDeliveryInstructions();
